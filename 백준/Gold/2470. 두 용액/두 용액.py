469a275be26a298e45d7113739e5672c02df059a
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2023-12-03 17:03:40 +0900

    [Gold V] Title: 두 용액, Time: 144 ms, Memory: 126372 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,27 @@
+import sys
+input=sys.stdin.readline
+
+n = int(input())
+solutions = list(map(int,input().split()))
+solutions.sort()
+
+m = sys.maxsize # 찾고자 하는 부분합 M
+
+answer = []
+interval_sum = 0
+
+start = 0
+end = n-1
+
+# start를 차례대로 증가시키며 반복
+while start < end:
+    interval_sum = solutions[start] + solutions[end]
+    if abs(interval_sum) < m:
+        m = abs(interval_sum)
+        answer = [solutions[start], solutions[end]]
+    if interval_sum < 0:
+        start += 1
+    else:
+        end -= 1
+
+print(*answer)
