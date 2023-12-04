4e677730d88e44e5db8ad0f3b4f80bac07431017
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2023-12-04 13:56:01 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 136 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,21 +1,18 @@
-# commit [BOJ] 2346_풍선 터뜨리기 / 실버3 / 10분 / O
 from collections import deque
 n = int(input())
-balloons = deque(list(map(int,input().split())))
-# 풍선 인덱스
-balloon_idx = deque([i for i in range(1, n+1)])
+balloons = deque(enumerate(list(map(int,input().split()))))
 idx = []
 
 while balloons:
-    move = balloons.popleft()
-    idx.append(balloon_idx.popleft())
+    b = balloons.popleft()
+    move = b[1] # 풍선 안 종이의 값
+    idx.append(b[0]+1) # 풍선의 인덱스
+    
     # 풍선 안 종이의 값이 양수인 경우 시계방향으로 (값-1)만큼 회전
     if move > 0:
         balloons.rotate(-(move-1))
-        balloon_idx.rotate(-(move-1))
     # 풍선 안 종이의 값이 음수인 경우 반시계방향으로 (값)만큼 회전
     else:
         balloons.rotate(-move)
-        balloon_idx.rotate(-move)
         
 print(*idx)
