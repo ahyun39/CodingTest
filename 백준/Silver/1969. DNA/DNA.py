5f830da2da7782c8bb1c756b8e1fdc89652b4c95
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-02-05 13:55:17 +0900

    [Silver IV] Title: DNA, Time: 160 ms, Memory: 114012 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.py
+++ 백준/Silver/1969. DNA/DNA.py
@@ -2,17 +2,13 @@ from collections import Counter
 
 N, M = map(int,input().split())
 DNA = [str(input()) for _ in range(N)]
-
-s = ['d' for _ in range(M)]
-hamming_dist = 0
+s, hamming_dist = '', N*M
 
 for i in range(M):
     alpha = []
     for j in DNA:
         alpha.append(j[i])
-    a = Counter(alpha)
-    a = sorted(a.items(),key=lambda x:(-x[1],x[0]))
-    s[i] = a[0][0]
-    hamming_dist += (N - a[0][1])
+    a = sorted(Counter(alpha).items(),key=lambda x:(-x[1],x[0]))
+    s, hamming_dist = s + a[0][0], hamming_dist - a[0][1]
 print(''.join(s))
 print(hamming_dist)
