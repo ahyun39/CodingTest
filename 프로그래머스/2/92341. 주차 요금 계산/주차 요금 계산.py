f7855e720700618a9b6add8abbdc8bbc1f8cae3e
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-02-16 16:54:15 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -2,38 +2,33 @@ import math
 
 def solution(fees, records):
     answer = []
-    #fees : 기본 시간, 기본 요금, 단위 시간, 단위 요금
-    car_parking_time = {}
-    in_out = []
-    total_parking_time = {}
+    entry_exit_time = {} # 입/출차 내역 시간 계산
+    in_out = [] # 출차된 내역이 없는 차를 확인하기 위한 리스트
+    total_parking_time = {} # 누적 주차 시간 계산
+    
     for record in records:
-        time, car, in_or_out = map(str,record.split())
-        if in_or_out == "IN":
+        time, car, in_or_out = map(str, record.split())
+        timestamp = int(time[:2]) * 60 + int(time[3:])
+        if in_or_out == "IN": # 입차
             in_out.append(car)
-            start_time = int(time[0:2])*60 + int(time[3:])
-            car_parking_time[car] = start_time
-        else:
+            entry_exit_time[car] = timestamp
+        else: # 출차
             in_out.remove(car)
-            end_time = int(time[0:2])*60 + int(time[3:])
-            car_parking_time[car] = end_time - car_parking_time[car]
-            if car in total_parking_time.keys():
-                total_parking_time[car] += car_parking_time[car]
-            elif car not in total_parking_time.keys():
-                total_parking_time[car] = car_parking_time[car]
-    print(car_parking_time)
-    if in_out:
+            entry_exit_time[car] = timestamp - entry_exit_time[car]
+            total_parking_time[car] = total_parking_time.get(car,0) + entry_exit_time[car]
+    if in_out: # 출차 기록이 없는 차량
         for car in in_out:
-            end_time = 23*60 + 59
-            car_parking_time[car] = end_time - car_parking_time[car]
-            if car in total_parking_time.keys():
-                total_parking_time[car] += car_parking_time[car]
-            elif car not in total_parking_time.keys():
-                total_parking_time[car] = car_parking_time[car]
-    car_total_time = sorted(total_parking_time.items(),key=lambda x:x[0])
+            end_time = 23 * 60 + 59
+            entry_exit_time[car] = end_time - entry_exit_time[car]
+            total_parking_time[car] = total_parking_time.get(car,0) + entry_exit_time[car]
+    
+    car_total_time = sorted(total_parking_time.items(), key=lambda x: x[0])
+    # fees: 기본 시간[0], 기본 요금[1], 단위 시간[2], 단위 요금[3]
     for total_time in car_total_time:
-        if total_time[1] <= fees[0]: answer.append(fees[1])
+        if total_time[1] <= fees[0]:
+            answer.append(fees[1])
         else:
-            fee = fees[1] + math.ceil((total_time[1] - fees[0])/fees[2]) * fees[3]
+            additional_time = total_time[1] - fees[0]
+            fee = fees[1] + math.ceil(additional_time / fees[2]) * fees[3]
             answer.append(fee)
-
     return answer
