b8ec845cd2a3dd147759ec6878850dd12606536d
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-02-19 18:01:58 +0900

    [Silver III] Title: 대표 자연수, Time: 2508 ms, Memory: 208492 KB -BaekjoonHub

--- 백준/Silver/2548. 대표 자연수/대표 자연수.py
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.py
@@ -1,18 +1,7 @@
-n = int(input())
-numbers = list(map(int,input().split()))
-numbers = sorted(numbers)
-min_sum = float('inf')
-answer = 0
-    
-for i in range(1,max(numbers)+1):
-    represent_num = i
-    sub_list = []
-    for number in numbers:
-        sub_list.append(abs(represent_num - number))
-    sub = sum(sub_list)
-    if min_sum == sub:
-        break
-    if min_sum > sub:
-        min_sum = sub
-        answer = represent_num
-print(answer)
+N = int(input())
+numbers = sorted(list(map(int,input().split())))
+# numbers 내 모든 자연수들의 차이 합을 담은 리스트
+min_diff = [sum([abs(i-j) for j in numbers]) for i in numbers]
+# 대표 자연수 : 차이 합이 가장 작은 자연수
+min_represent_num = numbers[min_diff.index(min(min_diff))]
+print(min_represent_num)
