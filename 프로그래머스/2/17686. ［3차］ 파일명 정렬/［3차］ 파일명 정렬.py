fbe7f9912f139b6af7a3f4c60c6275cc1497d6ae
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-02-22 17:38:07 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 9.54 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,20 +1,17 @@
 def solution(files):
-    answer = []
     split_files = []
     for file in files:
-        idx = []
-        is_num = 0
-        for i in range(len(file)):
-            if file[i].isdigit() and is_num == 0:
-                is_num = 1
-                idx.append(i)
-            elif not file[i].isdigit() and is_num == 1:
-                idx.append(i)
-                break
-        if len(idx) > 1:
-            split_files.append([file[:idx[0]], file[idx[0]:idx[1]], file[idx[1]:]])
-        else:
-            split_files.append([file[:idx[0]],file[idx[0]:]])
-    split_files = sorted(split_files, key=lambda x:(x[0].lower(),int(x[1]),files.index(''.join(x))))
-    answer = [''.join(f) for f in split_files]
-    return answer
+        head, number, tail = "", "", ""
+        for i, c in enumerate(file):
+            if c.isdigit():
+                number += c
+            else:
+                if number:
+                    tail = file[i:]
+                    break
+                else:
+                    head += c
+        split_files.append([head, number, tail])
+    
+    split_files.sort(key=lambda x: (x[0].lower(), int(x[1]), files.index(''.join(x))))
+    return [''.join(f) for f in split_files]
