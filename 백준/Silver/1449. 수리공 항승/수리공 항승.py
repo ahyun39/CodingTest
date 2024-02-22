90378841beeefca17f7d999620d2abdcf9fd0665
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-02-22 14:38:55 +0900

    [Silver III] Title: 수리공 항승, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.py
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -1,24 +1,8 @@
-import math
-
-n, l = map(int,input().split())
-pipes = list(map(int,input().split()))
-pipes.sort()
-
-# 테이프 시작 위치
-start = pipes[0]-0.5
-# 테이프가 끝나는 위치
-end = start + l
-# 처음 지점에서 테이프를 사용하기 때문에 정답값은 1개 이상이 됨.
-ans = 1
-
-for i in range(0, n):
-    # 물 새는 곳이 테이프 내에 있는 경우
-    if start < pipes[i] < end:
-        continue
-    # 테이프가 벗어난 위치에 물이 새는 경우
+N, L = map(int,input().split())
+water = sorted(list(map(int,input().split())))
+start, cnt = water[0] - 0.5, 1
+for i in range(1,N):
+    if start + L >= water[i] + 0.5: continue
     else:
-        ans += 1
-        start = pipes[i] - 0.5
-        end = start + l
-
-print(ans)
+        start, cnt = water[i] - 0.5, cnt + 1
+print(cnt)
