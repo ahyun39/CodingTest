014bc03d78e7afefbabeaa3325cd6fd0b8426bad
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-03-04 18:42:50 +0900

    [Gold V] Title: 배열 돌리기 3, Time: 212 ms, Memory: 115428 KB -BaekjoonHub

--- 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.py
+++ 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.py
@@ -1,30 +1,5 @@
-def one(board):
-    for i in range(n//2):
-        temp = board[i]
-        board[i] = board[n-1-i]
-        board[n-1-i] = temp
-    return board
-def two(board,n,m):
-    for i in range(m//2):
-        left = [board[j][i] for j in range(n)]
-        right = [board[j][m-1-i] for j in range(n)]
-        for j in range(n):
-            board[j][i] = right[j]
-            board[j][m-1-i] = left[j]
-    return board
-def three(board,n,m):
-    re_board = [[0 for _ in range(n)] for _ in range(m)]
-    for i in range(n):
-        for j in range(m):
-            re_board[j][n-1-i] = board[i][j]
-    return re_board, m, n
-def four(board,n,m):
-    re_board = [[0 for _ in range(n)] for _ in range(m)]
-    for i in range(n):
-        for j in range(m):
-            re_board[m-1-j][i] = board[i][j]
-    return re_board, m, n
 def five(board,n,m):
+    n, m = len(board), len(board[0])
     re_board = []
     g1 = [board[i][:m//2] for i in range(n//2)]
     g2 = [board[i][m//2:] for i in range(n//2)]
@@ -34,9 +9,10 @@ def five(board,n,m):
         re_board.append(g3[i] + g1[i])
     for i in range(len(g1)):
         re_board.append(g4[i] + g2[i])
-    
     return re_board
+
 def six(board,n,m):
+    n, m = len(board), len(board[0])
     re_board = []
     g1 = [board[i][:m//2] for i in range(n//2)]
     g2 = [board[i][m//2:] for i in range(n//2)]
@@ -53,10 +29,10 @@ board = [list(map(int,input().split())) for _ in range(n)]
 todo = list(map(int,input().split()))
 
 for i in todo:
-    if i == 1: result = one(board)
-    elif i == 2: result = two(board,n,m)
-    elif i == 3: result, n, m = three(board,n,m)
-    elif i == 4: result, n, m = four(board,n,m)
+    if i == 1: result = [board[len(board) - 1 - i] for i in range(len(board))]
+    elif i == 2: result = [row[::-1] for row in board]
+    elif i == 3: result = [[board[i][j] for i in range(len(board))][::-1] for j in range(len(board[0]))]
+    elif i == 4: result = [[board[i][j] for i in range(len(board))] for j in range(len(board[0]) - 1, -1, -1)]
     elif i == 5: result = five(board,n,m)
     elif i == 6: result = six(board,n,m)
     board = result
