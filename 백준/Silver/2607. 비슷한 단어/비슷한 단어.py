1d42f98e207fbe6f20764a2b2afd1e8cc6109eaa
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-03-04 17:13:00 +0900

    [Silver II] Title: 비슷한 단어, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -1,9 +1,8 @@
 n = int(input())
-words = [sorted(list(str(input()))) for _ in range(n)]
+words = [list(str(input())) for _ in range(n)]
 ans = 0
 for i in range(1,n):
-    t = words[i].copy()
-    k = words[0].copy()
+    t, k = words[i].copy(), words[0].copy()
     for j in range(len(words[i])):
         if words[i][j] in k: 
             k.remove(words[i][j])
