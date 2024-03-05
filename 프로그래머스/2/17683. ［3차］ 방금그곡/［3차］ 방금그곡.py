ce81e8a7557d36276afcd11f77e98f8b9c41d2f0
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-03-05 17:08:06 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -1,17 +1,23 @@
 def solution(m, musicinfos):
-    answer = ''
-    musicinfos = [list(map(str,i.replace("C#","H").replace("D#","I").replace("F#","J").replace("G#","K").replace("A#","L").replace("B#","M").split(","))) for i in musicinfos]
-    m = m.replace("C#","H").replace("D#","I").replace("F#","J").replace("G#","K").replace("A#","L").replace("B#","M")
-    play = 0
+    # '#'을 다른 문자로 치환하는 함수
+    def replace_sharp(s):
+        return s.replace("C#", "H").replace("D#", "I").replace("F#", "J").replace("G#", "K").replace("A#", "L").replace("B#", "M")
+
+    musicinfos = [list(map(str, replace_sharp(i).split(","))) for i in musicinfos]
+    m = replace_sharp(m)
+
+    play_time = 0
+    play_music = ''
 
     for info in musicinfos:
         start_time = int(info[0][:2]) * 60 + int(info[0][3:])
         end_time = int(info[1][:2]) * 60 + int(info[1][3:])
         time = end_time - start_time
 
-        music = info[3]*(time//len(info[3])) + info[3][:int(time % len(info[3]))]
-        if m in music and time > play:
-            answer = info[2]
-            play = time
-    if answer == '': answer = '(None)'
-    return answer
+        music = info[3] * (time // len(info[3])) + info[3][:int(time % len(info[3]))]
+
+        if m in music and time > play_time:
+            play_music = info[2]
+            play_time = time
+
+    return play_music if play_music else '(None)'
