423db05158ed0c837f4b7962bbd41742f7917fb1
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-03-06 20:49:55 +0900

    [level 2] Title: 방문 길이, Time: 1.97 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,35 +1,15 @@
 def solution(dirs):
     answer = 0
-    directions = [[0,1],[0,-1],[1,0],[-1,0]]
+    directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
     x, y = 0, 0
     lanes = []
-    for i in range(len(dirs)):
-        if dirs[i] == "U":
-            if -5 <= x + directions[0][0] <= 5 and -5 <= y + directions[0][1] <= 5:
-                nx, ny = x + directions[0][0], y + directions[0][1]
-                if [x, y, nx, ny] not in lanes and [nx, ny, x, y] not in lanes:
-                    answer += 1
-                    lanes.append([x, y, nx, ny])
-                x, y = nx, ny
-        elif dirs[i] == "D":
-            if -5 <= x + directions[1][0] <= 5 and -5 <= y + directions[1][1] <= 5:
-                nx, ny = x + directions[1][0], y + directions[1][1]
-                if [x, y, nx, ny] not in lanes and [nx, ny, x, y] not in lanes:
-                    answer += 1
-                    lanes.append([x, y, nx, ny])
-                x, y = nx, ny
-        elif dirs[i] == "R":
-            if -5 <= x + directions[2][0] <= 5 and -5 <= y + directions[2][1] <= 5:
-                nx, ny = x + directions[2][0], y + directions[2][1]
-                if [x, y, nx, ny] not in lanes and [nx, ny, x, y] not in lanes:
-                    answer += 1
-                    lanes.append([x, y, nx, ny])
-                x, y = nx, ny
-        elif dirs[i] == "L":
-            if -5 <= x + directions[3][0] <= 5 and -5 <= y + directions[3][1] <= 5:
-                nx, ny = x + directions[3][0], y + directions[3][1]
-                if [x, y, nx, ny] not in lanes and [nx, ny, x, y] not in lanes:
-                    answer += 1
-                    lanes.append([x, y, nx, ny])
-                x, y = nx, ny
+
+    for d in dirs:
+        dx, dy = directions[int(d.replace("U","0").replace("D","1").replace("R","2").replace("L","3")) % 4]
+        if -5 <= x + dx <= 5 and -5 <= y + dy <= 5:
+            nx, ny = x + dx, y + dy
+            if (x, y, nx, ny) not in lanes and (nx, ny, x, y) not in lanes:
+                answer += 1
+                lanes.append((x, y, nx, ny))
+            x, y = nx, ny
     return answer
