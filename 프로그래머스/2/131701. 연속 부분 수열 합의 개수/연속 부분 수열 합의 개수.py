f69ab46385a2a1cae8ff05dec1314fcc790addbd
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-03-13 23:13:22 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 5025.18 ms, Memory: 43.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,9 @@
+from itertools import combinations
+def solution(elements):
+    s = set(elements)
+    l = len(elements)
+    elements = elements + elements + elements
+    for i in range(1,l):
+        for j in range(i,l+i):
+            s.add(sum(elements[j-i:j+1]))
+    return len(s)
