a257dda8802cec858b4aa8c7cab4cd408d72fb84
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-03-19 14:38:57 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.48 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -5,10 +5,9 @@ def solution(orders, course):
     for n in course:
         set_menu = {}
         for order in orders:
-            order = ''.join(sorted(list(order)))
-            for c in combinations(order,n):
+            for c in combinations(sorted(order), n):
                 menu = ''.join(c)
-                set_menu[menu] = set_menu.get(menu,0) + 1
-        answer += [k for k,v in set_menu.items() if max(set_menu.values()) == v and v >= 2]
-    answer.sort()
-    return answer
+                set_menu[menu] = set_menu.get(menu, 0) + 1
+        max_count = max(set_menu.values()) if set_menu else 0
+        answer += [k for k, v in set_menu.items() if v == max_count and v >= 2]
+    return sorted(answer)
