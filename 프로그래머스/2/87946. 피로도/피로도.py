b4c0179c71ab380952de69dd557ab10744629662
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-03-21 20:45:32 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -3,16 +3,10 @@ from itertools import permutations
 def solution(k, dungeons):
     answer = -1
     for permu in permutations(dungeons,len(dungeons)):
-        fatigue = k
-        cnt = 0
+        fatigue, cnt = k, 0
         for p in permu:
-            if fatigue >= p[0]:
-                fatigue -= p[1]
-                cnt += 1
-            else:
-                break
-        if cnt > answer:
-            answer = cnt
-        if answer == len(dungeons):
-            break
+            if fatigue >= p[0]: fatigue, cnt = fatigue - p[1], cnt + 1
+            else: break
+        if cnt > answer: answer = cnt
+        if answer == len(dungeons): break
     return answer
