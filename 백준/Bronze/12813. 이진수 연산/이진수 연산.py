c99ae067652cfe363d4401c24ef093b06d15ad9c
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-04-20 15:04:34 +0900

    [Bronze II] Title: 이진수 연산, Time: 120 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/12813. 이진수 연산/이진수 연산.py
@@ -0,0 +1,8 @@
+a, b = int(input(),2), int(input(),2)
+n = 100000
+mask = 2 ** n - 1
+print(bin(a & b)[2:].zfill(n))
+print(bin(a | b)[2:].zfill(n))
+print(bin(a ^ b)[2:].zfill(n))
+print(bin(a ^ mask)[2:].zfill(n))
+print(bin(b ^ mask)[2:].zfill(n))
