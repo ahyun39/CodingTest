c61c153a26cf653b7bcb8a72a17569d38df9da6f
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-05-07 10:07:01 +0900

    [Silver IV] Title: 문자열 집합, Time: 332 ms, Memory: 117980 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -2,12 +2,8 @@ n, m = map(int,input().split())
 ans = 0
 words = {}
 for _ in range(n):
-    word = str(input())
-    words[word] = 0
+    words[input()] = 0
 for _ in range(m):
-    word = str(input())
-    if word in words:
-        words[word] += 1
-for v in words.values():
-    if v >= 1: ans += v
-print(ans)
+    word = input()
+    if word in words: words[word] += 1
+print(sum(v for v in words.values() if v >= 1))
