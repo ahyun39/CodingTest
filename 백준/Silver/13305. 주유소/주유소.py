afa8eafce6bdb7961eec6b6ef0e6fb8b9e4e0352
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-05-13 15:57:07 +0900

    [Silver III] Title: 주유소, Time: 176 ms, Memory: 136396 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,14 +1,12 @@
 n = int(input())
-distances = list(map(int,input().split())) + [0]
+distances = list(map(int,input().split()))
 oils = list(map(int,input().split()))
 
-# dp[i]: i번째 주유소까지 도달하는데 필요한 최소 비용
-dp = [float('inf')] * (n+1)
-dp[0] = 0
+min_price = oils[0]
+total = 0
 
-for i in range(n):
-    cost = 0
-    for j in range(i, n):
-        cost += distances[j] * oils[i]
-        dp[j + 1] = min(dp[j + 1], dp[i] + cost)
-print(dp[n])
+for i in range(n-1):
+    if min_price > oils[i]:
+        min_price = oils[i]
+    total += (min_price*distances[i])
+print(total)
