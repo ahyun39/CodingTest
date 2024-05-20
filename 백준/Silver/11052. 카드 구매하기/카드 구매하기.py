f800256b5179f227fef247aef5b90a69193d32fd
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-05-20 16:26:27 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -2,7 +2,7 @@ n = int(input())
 cards = [0] + list(map(int,input().split()))
 dp = [0] * (n+1)
 cards_idx = [[i,v] for i, v in enumerate(cards)]
-for i in range(1,n+1):
-    for j in range(i):
-        dp[i] = max(dp[i-j]+dp[j],cards[i],dp[i])
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + cards[j])
 print(dp[n])
