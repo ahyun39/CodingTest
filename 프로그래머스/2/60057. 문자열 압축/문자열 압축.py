d6b073ad50cbcc5c3651ed244e030222372ad7c9
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-06-04 16:19:46 +0900

    [level 2] Title: 문자열 압축, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -3,20 +3,19 @@ def solution(s):
     sList = []
     lenList = []
     for i in range(1,len(s)//2+1):
-        cutList = []
-        sameList = []
+        cutList = [] # 압축된 문자를 담은 리스트
+        sameList = [] # 단위별로 자른 문자의 개수를 찾기 위한 리스트
         for k in range(0,len(s),i):
-            if s[k:k+i] not in sameList:
-                sameList = []
-                sameList.append(s[k:k+i])
-                if s[k:k+i] != s[k+i:k+i+i]:
+            if s[k:k+i] not in sameList: # 현재 문자가 sameList에 없으면
+                sameList = [s[k:k+i]]
+                if s[k:k+i] != s[k+i:k+i+i]: # 다음 문자랑 현재 문자랑 다른 경우
                     cutList.append(s[k:k+i])
-            elif (s[k:k+i] in sameList) and (s[k:k+i] != s[k+i:k+i+i]):
-                sameList.append(s[k:k+i])
-                cutList.append(str(sameList.count(s[k:k+i]))+s[k:k+i])
-            elif (s[k:k+i] in sameList) and (s[k:k+i] == s[k+i:k+i+i]):
-                sameList.append(s[k:k+i])
-        sList.append(''.join(cutList))
+            elif (s[k:k+i] in sameList) and (s[k:k+i] != s[k+i:k+i+i]): # 현재 문자가 sameList에 있으면서 다음 문자가 현재 문자랑 다른 경우
+                sameList.append(s[k:k+i]) # sameList에 추가
+                cutList.append(str(sameList.count(s[k:k+i]))+s[k:k+i]) # 압축된 문자 구하기
+            elif (s[k:k+i] in sameList) and (s[k:k+i] == s[k+i:k+i+i]): # 현재 문자가 sameList에 있으면서 다음 문자가 현재 문자랑 같은 경우
+                sameList.append(s[k:k+i]) # sameList에 추가
+        sList.append(''.join(cutList)) # 압축된 문자를 합쳐서 문자열로 join해서 sList에 저장
     for i in sList:
         lenList.append(len(i))
     if len(s) == 1:
