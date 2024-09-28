c175c76d4f2d63c48c04bd3a9391852f4c4fa238
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-09-28 22:46:20 +0900

    [level 2] Title: 시소 짝꿍, Time: 6.98 ms, Memory: 13.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def solution(weights):
+    answer = 0
+    counter = Counter(weights)
+    for c in counter:
+        answer += counter[c] * (counter[c] - 1) // 2
+        answer += counter[c] * counter[c * 4 / 3]
+        answer += counter[c] * counter[c * 4 / 2]
+        answer += counter[c] * counter[c * 3 / 2]
+    return answer
