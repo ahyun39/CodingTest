729a51919bd6ec158294aad7f52d7440353eee1d
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-10-05 21:44:43 +0900

    [Silver III] Title: N과 M (4), Time: 120 ms, Memory: 111564 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,6 +1,18 @@
-from itertools import combinations_with_replacement
-
-n, m = map(int,input().split())
-nums = [i for i in range(1,n+1)]
-for c in combinations_with_replacement(nums,m):
-    print(*c)
+n, m = map(int, input().split())
+ans = []
+def back():
+    if len(ans) == m:
+        print(*ans)
+        return
+    for i in range(1, n+1):
+        if ans:
+            if ans[-1] <= i:
+                ans.append(i)
+                back()
+                ans.pop()
+        else:
+            ans.append(i)
+            back()
+            ans.pop()
+    return
+back()
