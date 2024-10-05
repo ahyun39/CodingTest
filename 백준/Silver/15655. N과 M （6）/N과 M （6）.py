4bb2fe118dd02296e0006bba984a35486da7d209
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-10-05 21:52:08 +0900

    [Silver III] Title: N과 M (6), Time: 92 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+ans = []
+
+def back():
+    if len(ans) == m:
+        print(*ans)
+        return
+    for i in range(n):
+        if ans:
+            if ans[-1] < nums[i]:
+                ans.append(nums[i])
+                back()
+                ans.pop()
+        else:
+            ans.append(nums[i])
+            back()
+            ans.pop()
+    return
+back()
