b0ab218bb7a8653ae684262f10f5a292efc76fc6
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-10-05 21:34:29 +0900

    [Silver III] Title: N과 M (2), Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,6 +1,13 @@
-from itertools import combinations
-
-n, m = map(int,input().split())
-nums = [i for i in range(1,n+1)]
-for c in combinations(nums,m):
-    print(*c)
+n, m = map(int, input().split())
+ans = []
+def back():
+    if len(ans) == m:
+        print(*ans)
+        return
+    for i in range(1, n+1):
+        if (len(ans) == 0 and i not in ans) or (len(ans) > 0 and max(ans) < i):
+            ans.append(i)
+            back()
+            ans.pop()
+    return
+back()
