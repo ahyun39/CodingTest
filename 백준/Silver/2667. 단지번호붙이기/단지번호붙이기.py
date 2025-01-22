2276a7d2be018a2a2e4a0bab6e386cc7fee41354
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-01-22 15:02:46 +0900

    [Silver I] Title: 단지번호붙이기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,23 +1,27 @@
-def dfs(graph, x, y, visited):
-    visited[x][y] = True
-    cnt = 1  # 현재 위치 포함
-    for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and graph[nx][ny] == '1':
-            cnt += dfs(graph, nx, ny, visited)  # 연결된 노드 탐색
-    return cnt
+def flood_fill(graph, x, y):
+    stack = [(x, y)]
+    size = 0
+    while stack:
+        cx, cy = stack.pop()
+        if graph[cx][cy] == '1':  # 방문 확인
+            graph[cx][cy] = '0'  # 방문 처리
+            size += 1
+            for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                nx, ny = cx + dx, cy + dy
+                if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == '1':
+                    stack.append((nx, ny))
+    return size
 
 def f():
     global N
     N = int(input())
-    graph = [input() for _ in range(N)]
-    visited = [[False] * N for _ in range(N)]
+    graph = [list(input()) for _ in range(N)]  # 문자열을 리스트로 변환
     apartment = []
 
     for i in range(N):
         for j in range(N):
-            if graph[i][j] == '1' and not visited[i][j]:
-                apartment.append(dfs(graph, i, j, visited))
+            if graph[i][j] == '1':
+                apartment.append(flood_fill(graph, i, j))
 
     apartment.sort()
     print(len(apartment))
