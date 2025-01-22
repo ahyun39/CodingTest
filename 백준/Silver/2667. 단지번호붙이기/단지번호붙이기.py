87ab1d751170d99fe1d06072a9836545d189c2e8
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-01-22 15:01:55 +0900

    [Silver I] Title: 단지번호붙이기, Time: 100 ms, Memory: 109708 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,30 +1,24 @@
-from collections import deque
-
-def bfs(graph, i, j, visited):
-    q = deque([(i, j)])
-    cnt = 1
-    while q:
-        x, y = q.popleft()
-        visited[x][y] = True
-        for dx, dy in [(-1,0), (0,1), (1,0), (0,-1)]:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and graph[nx][ny] == '1':
-                visited[nx][ny] = True
-                q.append((nx, ny))
-                cnt += 1
+def dfs(graph, x, y, visited):
+    visited[x][y] = True
+    cnt = 1  # 현재 위치 포함
+    for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and graph[nx][ny] == '1':
+            cnt += dfs(graph, nx, ny, visited)  # 연결된 노드 탐색
     return cnt
-                
+
 def f():
     global N
     N = int(input())
     graph = [input() for _ in range(N)]
-    
     visited = [[False] * N for _ in range(N)]
     apartment = []
+
     for i in range(N):
         for j in range(N):
             if graph[i][j] == '1' and not visited[i][j]:
-                apartment.append(bfs(graph, i, j, visited))
+                apartment.append(dfs(graph, i, j, visited))
+
     apartment.sort()
     print(len(apartment))
     print(*apartment, sep='\n')
