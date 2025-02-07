e9a3d200ba4522f1dd7513253bd4219d42f7023e
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-02-07 16:04:01 +0900

    [Gold V] Title: 치킨 배달, Time: 132 ms, Memory: 111440 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,19 +1,29 @@
+# 15686_치킨 배달_골드5
+
 from itertools import combinations
 
-n, m = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(n)]
+def f():
+    N, M = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
 
-homes = [(i,j) for i in range(n) for j in range(n) if graph[i][j] == 1]
-chickens = [(i,j) for i in range(n) for j in range(n) if graph[i][j] == 2]
-min_dist = float('inf')
-for comb in combinations(chickens, m):
-    dist_ = 0
-    for h in homes:
-        dist = float('inf')
-        for c in comb:
-            if dist > (abs(h[0] - c[0]) + abs(h[1] - c[1])):
-                dist = abs(h[0] - c[0]) + abs(h[1] - c[1])
-        dist_ += dist
-    if min_dist > dist_:
-        min_dist = dist_
-print(min_dist)
+    chickens = [(i+1, j+1) for i in range(N) for j in range(N) if board[i][j] == 2]
+    houses = [(i+1, j+1) for i in range(N) for j in range(N) if board[i][j] == 1]
+
+    min_dist = float('inf')
+
+    for c in combinations(chickens, M):
+        dist = 0
+        for house in houses:
+            hx, hy = house
+            house_to_chicken = 101
+            for chicken in c:
+                cx, cy = chicken
+                d = abs(cx-hx) + abs(cy-hy)
+                if house_to_chicken > d:
+                    house_to_chicken = d
+            dist += house_to_chicken
+        if min_dist > dist:
+            min_dist = dist
+    print(min_dist)
+
+f()
