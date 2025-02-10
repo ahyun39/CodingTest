f0b7ee01e6bdd67b986879067754e2db0f7ecc7e
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-02-10 13:20:17 +0900

    [Bronze I] Title: 고양이는 많을수록 좋다, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/27961. 고양이는 많을수록 좋다/고양이는 많을수록 좋다.py
+++ 백준/Bronze/27961. 고양이는 많을수록 좋다/고양이는 많을수록 좋다.py
@@ -1,14 +1,8 @@
 def f():
     N = int(input())
-    cnt = 0
-    madoka_cat = 0
-    while True:
-        if madoka_cat == N:
-            break
-        if madoka_cat + 1 > madoka_cat * 2:
-            madoka_cat += 1
-        else:
-            madoka_cat = min(N, madoka_cat * 2)
+    cnt, madoka_cat = 0, 0
+    while madoka_cat < N:
+        madoka_cat = min(N, max(madoka_cat + 1, madoka_cat * 2))
         cnt += 1
     return cnt
 
