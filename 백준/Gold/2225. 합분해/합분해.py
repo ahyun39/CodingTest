348f2026c77b891f121b26827979822a24469d1c
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-02-20 19:13:33 +0900

    [Gold V] Title: 합분해, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,12 +1,17 @@
-n, k = map(int, input().split())
-dp = [[0] * (n + 1) for _ in range(k + 1)]
+def f():
+    N, K = map(int, input().split())
+    dp = [[0] * (N + 1) for _ in range(K + 1)]
 
-dp[0][0] = 1
+    # k=1일 때 초기값 설정
+    for n in range(N + 1):
+        dp[1][n] = 1
 
-for i in range(1, k + 1):
-    for j in range(n + 1):
-        dp[i][j] = 0
-        for x in range(n + 1):
-            if j >= x:
-                dp[i][j] += dp[i - 1][j - x]
-print(dp[k][n] % 1000000000)
+    # DP 계산 (누적 합 활용)
+    for k in range(2, K + 1):
+        dp[k][0] = 1  # n=0인 경우, 한 가지 방법만 존재
+        for n in range(1, N + 1):
+            dp[k][n] = (dp[k][n-1] + dp[k-1][n]) % 1000000000
+
+    print(dp[K][N])
+
+f()
