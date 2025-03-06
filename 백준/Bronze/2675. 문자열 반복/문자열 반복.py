301ab3fed54d4f497454fa3aa2157d004fa78ad3
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-03-07 00:16:12 +0900

    [Bronze II] Title: 문자열 반복, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,7 +1,12 @@
 def main():
-    for _ in range(int(input())):
-        R, S = input().split()
-        print(''.join(s * int(R) for s in S))
+    T = int(input())
+
+    for _ in range(T):
+        R, S = map(str, input().split())
+        P = ""
+        for s in S:
+            P += s * int(R)
+        print(P)
 
 if __name__ == "__main__":
     main()
