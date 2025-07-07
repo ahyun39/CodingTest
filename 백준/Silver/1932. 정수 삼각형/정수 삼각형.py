fb53341f5f9ffe77f0a2925e17a6003fb7f17b76
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-07-07 22:22:22 +0900

    [Silver I] Title: 정수 삼각형, Time: 116 ms, Memory: 111500 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,8 @@
+n = int(input())
+nums = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n-2, -1, -1):
+    for j in range(i+1):
+        nums[i][j] += max(nums[i+1][j], nums[i+1][j+1])
+
+print(nums[0][0])
