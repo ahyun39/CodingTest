bb6b7c34c323fa343fb9178d7045aee1cfe6684e
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-08-08 21:21:49 +0900

    [Silver II] Title: DFS와 BFS, Time: 152 ms, Memory: 115344 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,36 +1,39 @@
+# 1260_DFS와 BFS_실버2
+
 from collections import deque
 
-def dfs(graph, v, visited):
-    visited[v] = True
-    print(v, end = ' ')
-    for i in graph[v]:
+def dfs(graph, start, visited):
+    visited[start] = True
+    print(start, end =' ')
+    for i in graph[start]:
         if not visited[i]:
             dfs(graph, i, visited)
 
-def bfs(graph, V, N):
-    visited = [False] * (N + 1)
-    q = deque([V])
-    visited[V] = True
+def bfs(graph, start, visited):
+    q = deque([start])
+    visited[start] = True
     while q:
-        x = q.popleft()
-        print(x, end = ' ')
-        for i in graph[x]:
+        v = q.popleft()
+        print(v, end =' ')
+        for i in graph[v]:
             if not visited[i]:
                 q.append(i)
                 visited[i] = True
 
-def f():
-    N, M, V = map(int, input().split())
-    graph = [[] for _ in range(N+1)]
-    for _ in range(M):
-        a, b = map(int, input().split())
-        graph[a].append(b)
-        graph[b].append(a)
-    for i in range(N+1):
-        graph[i].sort()
-    visited = [False] * (N+1)
-    dfs(graph, V, visited)
-    print()
-    bfs(graph, V, N)
+N, M, V = map(int, input().split())
+graph = [[] for i in range(N+1)]
 
-f()
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(N+1):
+    graph[i].sort()
+
+visited = [False] * (N+1)
+dfs(graph, V, visited)
+print()
+
+visited = [False] * (N+1)
+bfs(graph, V, visited)
