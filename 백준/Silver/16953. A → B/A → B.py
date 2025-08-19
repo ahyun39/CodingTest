35622454f666cc109e8184c9c909a00ff24a003d
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-08-19 22:32:26 +0900

    [Silver II] Title: A → B, Time: 116 ms, Memory: 112772 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,13 +1,7 @@
-# 16953_A -> B_실버2
-
-from collections import deque, defaultdict
+from collections import deque
 
 a, b = map(int, input().split())
-
-visited = defaultdict(int)
-
 q = deque([(a, 1)])
-visited[a] = 0
 ans = -1
 
 while q:
@@ -15,15 +9,8 @@ while q:
     if num == b:
         ans = cnt
         break
-    
-    mul_two, add_one = num * 2, int(str(num) + '1')
-    if mul_two <= b:
-        if visited[mul_two] > cnt:
-            visited[mul_two] = cnt
-        q.append((mul_two, cnt+1))
-    if add_one <= b:
-        if visited[add_one] > cnt:
-            visited[add_one] = cnt
-        q.append((add_one, cnt+1))
+    for nxt in (num*2, int(str(num)+'1')):
+        if nxt <= b:
+            q.append((nxt, cnt+1))
 
 print(ans)
