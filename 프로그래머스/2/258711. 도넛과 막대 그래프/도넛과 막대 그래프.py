22e7fd48bb4243715416a417d6c5544c7c6697a8
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-10-04 22:14:21 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1023.84 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -7,12 +7,14 @@ def solution(edges):
     for i, j in edges:
         dic[i][0] += 1 # 나가는 간선
         dic[j][1] += 1 # 들어오는 간선
+        
     for node, item in dic.items():
         out_ = item[0]
         in_ = item[1]
         if out_ >= 2 and in_ == 0: answer[0] = node
         elif out_ >= 2 and in_ >= 2: answer[3] += 1
         elif out_ == 0 and in_ >= 1: answer[2] += 1
-    t = dic[answer[0]][0]
+        
+    t = dic[answer[0]][0] # 생성한 정점에 연결된 그래프
     answer[1] = t - answer[2] - answer[3]
     return answer
