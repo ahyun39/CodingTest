608b01b2a05bcc40d33fd3b7b6b88eb0fe6dfcbf
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-10-04 15:48:28 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,8 +1,5 @@
 def solution(n, arr1, arr2):
-    answer = []
-    bin_map = []
-    for i in range(n):
-        to_bin = str(bin(arr1[i] | arr2[i])).replace('b','')[-n:].replace('1','#').replace('0',' ')
-        bin_to_str = ' '*(n-len(to_bin)) + to_bin
-        answer.append(bin_to_str)
-    return answer
+    return [
+        bin(a | b)[2:].zfill(n).replace('1', '#').replace('0', ' ')
+        for a, b in zip(arr1, arr2)
+    ]
