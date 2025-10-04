926d08c0476ffdba483adc1bab2ddd1a3cdb18b8
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-10-04 17:50:35 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.47 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,25 +1,28 @@
 def solution(friends, gifts):
-    answer = 0
-    next_month_gift = []
-    gift_record = {friend:{friend:0 for friend in friends} for friend in friends}
-    gift_cnt = {friend:0 for friend in friends}
-    
+    from collections import defaultdict
+
+    gift_record = {f: defaultdict(int) for f in friends}
+    gift_cnt = {f: 0 for f in friends}
+
+    # 선물 기록 업데이트
     for gift in gifts:
-        give, receive = map(str, gift.split())
+        give, receive = gift.split()
         gift_record[give][receive] += 1
         gift_cnt[give] += 1
         gift_cnt[receive] -= 1
+
+    next_month_gift = []
     
+    # 다음 달 예상 선물 수 계산
     for friend in friends:
-        give_take = gift_record[friend]
-        cnt = 0
-        for f in friends:
-            if friend != f:
-                if give_take[f] > gift_record[f][friend]:
-                    cnt += 1
-                elif give_take[f] == gift_record[f][friend]:
-                    if gift_cnt[friend] > gift_cnt[f]:
-                        cnt += 1
+        cnt = sum(
+            1
+            for f in friends
+            if f != friend and (
+                gift_record[friend][f] > gift_record[f][friend] or
+                (gift_record[friend][f] == gift_record[f][friend] and gift_cnt[friend] > gift_cnt[f])
+            )
+        )
         next_month_gift.append(cnt)
 
     return max(next_month_gift)
