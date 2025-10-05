0342d514cc95508186c3fe53f80066b984fc0d0c
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-10-05 18:06:41 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.21 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370#) 
 
 ### 성능 요약
 
-메모리: 9.69 MB, 시간: 4.14 ms
+메모리: 9.43 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 05일 18:04:29
+2025년 10월 05일 18:06:39
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,16 +1,16 @@
-from datetime import datetime
-
 def solution(today, terms, privacies):
+    # 오늘 날짜를 연,월,일로 분리
+    t_year, t_month, t_day = map(int, today.split('.'))
+    # 약관별 유효기간(일) 딕셔너리
+    term_dict = {k: int(v) * 28 for k, v in (t.split() for t in terms)}
+
     answer = []
-    # 문자열 -> 날짜 형식으로 변환
-    today = datetime.strptime(today, "%Y.%m.%d")
-    # 약관 보관 일자 딕셔너리
-    term_dict = {term_nm: int(month)*28 for term_nm, month in (term.split() for term in terms)}
-    
-    for i in range(len(privacies)):
-        date, term = map(str, privacies[i].split())
-        date = datetime.strptime(date, "%Y.%m.%d")
-        month_diff = ((today.year - date.year) * 12 + (today.month - date.month)) * 28 + (today.day - date.day)
-        if month_diff >= term_dict[term]:
-            answer.append(i+1)
+    for i, privacy in enumerate(privacies):
+        p_date, p_term = privacy.split()
+        p_year, p_month, p_day = map(int, p_date.split('.'))
+        # 총 일수 계산: 연, 월, 일 단위
+        total_days = (t_year - p_year) * 12 * 28 + (t_month - p_month) * 28 + (t_day - p_day)
+        if total_days >= term_dict[p_term]:
+            answer.append(i + 1)
+
     return answer
